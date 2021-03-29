98881f6b35c2a957621cf256a5f5337c155e6295
freitasartur <81202141+freitasartur@users.noreply.github.com>
2021-03-29 00:35:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
 port = 1883
-port = 1884
 topic = "/python/mqtt"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
